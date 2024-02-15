19317ceabe95d8008f979a7d6fd8cb5fffb35eed
Justin.d22 <justin.d22@gso.schule.koeln>
2024-02-15 09:49:49 +0100

    Sound Buttons erweitert

---
+++ Frogger/Frogger/Bahn.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frogger
+{
+    public class Bahn
+    {
+        private int spawnRate;
+
+        public int SpawnRate
+        {
+            get
+            {
+                return spawnRate;
+            }
+            set
+            {
+                spawnRate = value;
+            }
+        }
+
+
+    }
+}
